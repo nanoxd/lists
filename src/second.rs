aa144f8191ef4b92d218bc36f2d13d25488cbbb4
Fernando Paredes <nano@fdp.io>
2017-09-01 07:31:04 -0700

    [second] Add support for Iterator

--- src/second.rs
+++ src/second.rs
@@ -1,3 +1,17 @@
+pub struct Iter<'a, T: 'a> {
+  next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+  type Item = &'a T;
+  fn next(&mut self) -> Option<Self::Item> {
+    self.next.map(|node| {
+      self.next = node.next.as_ref().map(|node| &**node);
+      &node.elem
+    })
+  }
+}
+
 pub struct IntoIter<T>(List<T>);
 
 impl<T> Iterator for IntoIter<T> {
@@ -28,22 +42,24 @@ impl<T> List<T> {
 
   pub fn pop(&mut self) -> Option<T> {
     self.head.take().map(|node| {
-        let node = *node;
-        self.head = node.next;
-        node.elem
+      let node = *node;
+      self.head = node.next;
+      node.elem
     })
   }
 
   pub fn peek(&self) -> Option<&T> {
-    self.head.as_ref().map(|node| {
-      &node.elem
-    })
+    self.head.as_ref().map(|node| &node.elem)
   }
 
   pub fn peek_mut(&mut self) -> Option<&mut T> {
-    self.head.as_mut().map(|node| {
-      &mut node.elem
-    })
+    self.head.as_mut().map(|node| &mut node.elem)
+  }
+
+  pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+    Iter {
+      next: self.head.as_ref().map(|node| &**node),
+    }
   }
 
   pub fn into_iter(self) -> IntoIter<T> {
@@ -52,16 +68,16 @@ impl<T> List<T> {
 }
 
 impl<T> Drop for List<T> {
-    fn drop(&mut self) {
-        let mut cur_link = self.head.take();
-        // `while let` == "do this thing until this pattern doesn't match"
-        while let Some(mut boxed_node) = cur_link {
-            cur_link = boxed_node.next.take();
-            // boxed_node goes out of scope and gets dropped here;
-            // but its Node's `next` field has been set to None
-            // so no unbounded recursion occurs.
-        }
+  fn drop(&mut self) {
+    let mut cur_link = self.head.take();
+    // `while let` == "do this thing until this pattern doesn't match"
+    while let Some(mut boxed_node) = cur_link {
+      cur_link = boxed_node.next.take();
+      // boxed_node goes out of scope and gets dropped here;
+      // but its Node's `next` field has been set to None
+      // so no unbounded recursion occurs.
     }
+  }
 }
 
 struct Node<T> {
@@ -123,5 +139,18 @@ mod test {
     assert_eq!(iter.next(), Some(3));
     assert_eq!(iter.next(), Some(2));
     assert_eq!(iter.next(), Some(1));
+  }
+
+  #[test]
+  fn iter() {
+    let mut list = List::new();
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    let mut iter = list.iter();
+    assert_eq!(iter.next(), Some(&3));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.next(), Some(&1));
   }
 }
