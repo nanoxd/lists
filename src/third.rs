d42c3f7ff984bcc2dfd98098a45498cd5d19585b
Fernando Paredes <nano@fdp.io>
2017-09-02 17:29:08 -0700

    Start new list

---
+++ src/third.rs
@@ -0,0 +1,18 @@
+use std::rc::Rc;
+
+pub struct List<T> {
+  head: Link<T>,
+}
+
+type Link<T> = Option<Rc<Node<T>>>;
+
+struct Node<T> {
+  elem: T,
+  next: Link<T>,
+}
+
+impl<T> List<T> {
+  pub fn new() -> Self {
+    List { head: None }
+  }
+}
